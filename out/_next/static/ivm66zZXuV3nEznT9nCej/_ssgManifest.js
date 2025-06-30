f684afeb2fe361ef228154bdee2ab8097d7084c5
akashdhiman19 <133624586+akashdhiman19@users.noreply.github.com>
2025-06-30 15:16:40 +0530

    Delete out directory

--- out/_next/static/ivm66zZXuV3nEznT9nCej/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002Fblog","\u002Fblog\u002F[slug]","\u002Fblog\u002Fcategory\u002F[slug]","\u002Fblog\u002Ftag\u002F[slug]","\u002Fcase-study\u002F[slug]","\u002Fportfolio\u002F[slug]","\u002Fprojects\u002F[slug]","\u002Fservices\u002F[category]\u002F[slug]","\u002Fteam\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
